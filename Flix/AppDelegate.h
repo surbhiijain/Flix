3a1ec409b32e886153e4e32f925fb1145ae52b68
Surbhi Jain <surbhij@fb.com>
2021-06-23 08:48:47 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Surbhi Jain on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Flix
+//
+//  Created by Surbhi Jain on 6/23/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
